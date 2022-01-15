9dabc32299bbc174c0d01f40f233f9267b8c24cf
lindabettini <linda.bettini@hotmail.it>
2022-01-15 22:29:25 +0100

    Add event and let price

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ va applicato uno sconto del 40% per gli over 65.
 Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). 
 */
 
+// // 1. Rcupero tutti gli elementi html 
+
 // ° FORMS ELEMENT
-const nameField = document.getElementById('#name');
-const kmsField = document.getElementById('#kms');
-const ageField = document.getElementById('#age');
-const generateBtn = document.getElementById('#generate-btn');
-const resetBtn = document.getElementById('#reset-btn');
+const nameField = document.getElementById('name');
+const kmsField = document.getElementById('kms');
+const ageField = document.getElementById('age');
+const generateBtn = document.getElementById('generate-btn');
+const resetBtn = document.getElementById('reset-btn');
 
 // ° TICKET ELEMENTS
 const quoteSection = document.getElementById('ticket-quote');
@@ -24,43 +26,17 @@ const cpPlaceholder = document.getElementById('cp');
 const carriagePlaceholder = document.getElementById('carriage');
 const pricePlaceholder = document.getElementById('price');
 
-// ° Calcolare il prezzo totale del viaggio
+// // 2. Lego eventi ai bottoni
 
-let discountValue = 0;
+generateBtn.addEventListener('click', function () {
 
-generaBtn.addEventListener('click', function () {    
-    let kmValue = parseInt(km.value);
-    let totalPrice = kmValue * 0.21;      
-    // & Calcolare eventuali sconti del viaggio
-    if (passengerAge.value === 'minorenne'){              
-        discountValue = (totalPrice / 100) * 20;
-        totalPrice = totalPrice - discountValue;
-    }  else if (passengerAge.value === 'over-65'){              
-        discountValue = (totalPrice / 100) * 40;
-        totalPrice = totalPrice - discountValue;
-    }
-    const finalPrice = totalPrice.toFixed(2); 
-    console.log(passengerAge.value)
-    console.log(finalPrice)
-    preventivo.innerHTML = `
-        
-<div class="" id="stampa-nome"><strong>NOME PASSEGGERO</strong></br>${passengerName.value}</div>
-<div class="" id="offerta"><strong>Offerta</strong></br>Biglietto Standard</div>
-<div class="" id="carrozza"><strong>Carrozza</strong></br>10</div>
-<div class="" id="codice-treno"><strong>Codice CP</strong></br>556789</div>
-<div class="" id="costo"><strong>Costo biglietto</strong></br>Il costo del biglietto è: &euro; ${finalPrice}</div>
-`;
-});
+        // // 3. Recupero i valori
+        const nameValue = nameField.value;
+        const kmsValue = parseInt(kmsField.value);
+        const ageValue = ageField.value;
+        console.log(nameValue, kmsValue, ageValue);
 
-// ° Cancellare tutti i dati inseriti
-
-annullaBtn.addEventListener('click', function () {    
-    passengerName.value = '';
-    km.value = '';
-    passengerAge.value = '';
-    preventivo.innerHTML = ``;
-});
-
-
-
-
+        // // 4. Calcolo prezzo base
+        let price = 0.21 * kmsValue;
+        console.log(price);
+})
